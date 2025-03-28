e4be2e2dfcd751e8b4d408e5fbaf4313e155cf2a
Prof. Gurguri <157112070+profgurguri@users.noreply.github.com>
2025-03-28 08:43:14 -0300

    feat(comandos): renomeação de notas para tag

--- index.js
+++ index.js
@@ -3,20 +3,25 @@ const fs = require("fs");
 
 const versao = "0.0.1";
 
-function notas(file_path, hashtag = "") {
+function tag(file_path, hashtag = "", options) {
   const transcricao = fs.readFileSync(file_path, "utf8");
   const combinacoes = transcricao.match(
     new RegExp(`.*#${hashtag === "" ? "\\w*" : hashtag}.*\n`, "g")
   );
   const arr_combinacoes = combinacoes?.map((val, idx, arr) => val.trim("\n"));
-  console.log(arr_combinacoes);
+  
+  if (options.output) {
+    fs.writeFileSync(options.output, arr_combinacoes.join('\n'))
+  }
+  
+  arr_combinacoes.forEach(anotacao => console.log(anotacao))
 }
 
-function frequencia(file_path) {
-  const timestamp = "\\d{2,2}:\\d{2,2}:\\d{2,2}\\.\\d{3,3}";
-  const exp = `${timestamp},${timestamp}\\n([^:]*)`;
+
+function frequencia(file_path, options) {
+  const exp = /(\D.*):\s/g;
   const transcricao = fs.readFileSync(file_path, "utf8");
-  const regex = new RegExp(exp, "g");
+  const regex = exp;
   const combinacoes = transcricao.matchAll(regex);
   const arr_combinacoes = Array.from(combinacoes);
   const str_combinacoes = arr_combinacoes?.map((val, idx, arr) =>
@@ -26,14 +31,24 @@ function frequencia(file_path) {
   const arr_lista = Array.from(lista);
   const final_lista = arr_lista.map((nome) => nome.toUpperCase());
   final_lista.sort((a, b) => a.localeCompare(b));
-  console.log(final_lista);
+  
+  if (options.output) {
+    fs.writeFileSync(options.output, final_lista.join('\n'))
+  }
+
+  final_lista.forEach(nome => console.log(nome))
 }
 
 program.name("guri").version(versao);
 
-program.command("notas <transcricao> [topico]").action(notas);
-program.command("frequencia <transcricao>").action(frequencia);
+program
+  .command("tag <transcricao> [hashtag]")
+  .option('-o, --output <destino>','salva o resultado em arquivo')
+  .action(tag);
+
+program
+  .command("frequencia <transcricao>")
+  .option('-o, --output <destino>','salva o resultado em arquivo')
+  .action(frequencia);
 
 program.parse(process.argv);
-
-// command('alunos <file>').description('Listar alunos na frequência').action((file)=>console.log('nome do arquivo:', file))
